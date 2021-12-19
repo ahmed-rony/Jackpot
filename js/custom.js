4093c135163261d7d0eb54213be053bbd0149706
unknown <ahmedrony2101@gmail.com>
2021-12-19 12:37:05 +0600

    till pricing plan

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    // steps slick   ==============;
+    $('.steps-slider').slick({
+        arrows: false,
+        slidesToShow: 3,
+        autoplay: true,
+    });
+
+});
