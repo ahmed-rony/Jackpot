58fe53329cc84428d08bc809fc770dbe107ac951
unknown <ahmedrony2101@gmail.com>
2021-12-21 21:47:34 +0600

    jackpot complete

--- js/custom.js
+++ js/custom.js
@@ -7,4 +7,29 @@ $(document).ready(function(){
         autoplay: true,
     });
 
+    // accordion border   ==============;
+    $('.accordion-item').on('click',function(){
+        $(this).addClass('border').siblings().removeClass('border');
+    })
+
+    // scrollbox   ====================;
+    var $container = $('#game-rank'),
+    i = 1;
+
+   $container
+     .on('reach.scrollbox', function () {
+       if (i < 6) {
+           // emulate XHR
+           window.setTimeout(function () {
+            $container.append('<div class="test-div">This is a test div #' + i ++ + '</div>');
+            $container.scrollbox('up<a href="https://www.jqueryscript.net/time-clock/">date</a>'); // recalculate bar height and position
+           }, 300);
+        }
+    })
+     .scrollbox({
+       // position from bottom when reach.scrollbox will be triggered
+       buffer: 150 
+   });
+
+
 });
