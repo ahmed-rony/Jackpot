5709540e03e76306f85e8fa7c484a705e50684eb
unknown <ahmedrony2101@gmail.com>
2021-12-25 16:58:58 +0600

    jackpot

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,35 @@ $(document).ready(function(){
         arrows: false,
         slidesToShow: 3,
         autoplay: true,
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                dots: false,
+                arrows: false,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                dots: false,
+                arrows: false,
+              }
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                arrows: false,
+              }
+            },
+          ],
     });
 
     // accordion border   ==============;
@@ -30,6 +59,34 @@ $(document).ready(function(){
        // position from bottom when reach.scrollbox will be triggered
        buffer: 150 
    });
+
+
+   // scroll position   ==============;
+  $(window).on('scroll',function(){
+    var scrolling = $(this).scrollTop();
+
+    // back to top appearance   ===========;
+    if(scrolling > 200){
+      $('.back-to-top').fadeIn();
+    }else{
+      $('.back-to-top').fadeOut();
+      
+    }
+    // navbar positioning    ==============;
+    if(scrolling > 50){
+      $('nav').addClass('nav-fix');
+    }else{
+      $('nav').removeClass('nav-fix');
+    }
+
+  })
+
+   // back to top action   ===========;
+   $('.back-to-top').on('click',function(){
+     $('html,body').animate({
+       scrollTop: '0px',
+     }, 1500);
+   })
 
 
 });
